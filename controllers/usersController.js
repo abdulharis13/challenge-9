5ad2669676c9426810c8581556e45ccc5756c209
mohammad abdul haris <mabduh@MacBook-Air-mohammad.local>
2022-07-27 08:05:34 +0700

    fix-login-with-token, see-profile, get-data, update-data

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,72 +1,88 @@
 const {user, user_bio} = require("../models");
+const bcrypt = require("bcrypt")
 
   const getAllplayer = async (req, res) => {
     try { 
-      const player = await user.findAll({ 
+      const player = await user.findAll({
         include: user_bio
       });
     res.status(200).json({
       massege: "Profile",
       player
-    })
+    });
   } catch(err) {
     console.error(err.massege);
   }
   };
   
-  const seeProfile = async (req, res) => {
-    const profile = await user.findOne({
+  const getProfile = async (req, res) => {
+    const profile = [ await user.findOne({
+      model: {
+        include: user_bio
+      },
       where: {
         id: req.params.id,
       },
+      
     })
-    const profiles = await user_bio.findOne({
-      where: {
-        id: req.params.id
-      },
-    })
+  ]
+    // const profiles =[ 
+    //   await user_bio.findOne({
+    //     where: {
+    //       id_foreign: profile.id
+    //     },
+    //     attributes: ["nama", "socialMedia", "aboutMe"]
+    //   })
+    // ]
     res.status(200).json({
       message: "profile-page",
       profile,
-      profiles
     })
   }
 
   const getUpdateprofile = async (req, res) => {
-    const updateProfile = await user.findOne({
-      include: user_bio
+    const getUser = await user.findOne({
+      where:{
+        id: req.params.id
+      },
+      include:{
+        model: user_bio
+      }
     });
     res.status(200).json({
-      updateProfile,
+      getUser,
     }) 
   };
   
   const postUpdateUser = async (req, res) => {
-    const { username, nama, email, socialMedia, aboutMe  } = req.body;
+    const { username, password, email } = req.body;
     const query = {
       where: {
         id: req.params.id,
       },
     };
-    user.update({ username,email }, query);
-    user_bio.update(
+    const profile = user.update({ username, password, email }, query);
+    const profiles = user_bio.update( 
       {
-        nama,
-        socialMedia,
-        aboutMe,
-      },
-      query,
+        nama: req.body.nama,
+        socialMedia: req.body.socialMedia,
+        aboutMe: req.body.aboutMe,
+      }, {
+        where: {
+          id_foreign: req.params.id
+        }
+      }
     ); 
     res.status(200).json({
       massege: "data updated",
-      playerUser,
-      playerBio,
+      profile,
+      profiles
   })
 }
 
   module.exports = {
     getAllplayer,
-    seeProfile,
+    getProfile,
     getUpdateprofile,
     postUpdateUser
   }
